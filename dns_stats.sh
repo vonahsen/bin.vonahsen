467c80080abdc842a2028f591321de579474f74f
Barry Von Ahsen <barry@vonahsen.com>
2026-06-22 13:50:32 -0500

    shellcheck dns_stats and suppress some findings

--- dns_stats.sh
+++ dns_stats.sh
@@ -6,8 +6,13 @@ set -ue #o pipefail
 #set -x
 
 DNS_LOG='/var/log/pihole/pihole.log'
+# SC2034 (warning): QUERY_TYPE_FIELD appears unused. Verify use (or export if used externally).
+# yes, these are cruft
+# shellcheck disable=SC2034
 QUERY_TYPE_FIELD=5
+# shellcheck disable=SC2034
 QUERY_FIELD=6
+# shellcheck disable=SC2034
 CLIENT_FIELD=8
 
 GREP_STR="query\["
@@ -28,8 +33,12 @@ TODAY=$(${DATE} +%Y-%m-%d)
 QUERIES=$(${GREP} "${GREP_STR}" ${DNS_LOG})
 
 TQ=$(echo "${QUERIES}" | ${WC} -l)
+# SC2016 (info): Expressions don't expand in single quotes, use double quotes for that. <- these are awk quotes, not bash quotes
+# shellcheck disable=SC2016
 TQT=$(echo "${QUERIES}" | ${AWK} '{print $5}' | ${SORT} | ${UNIQ} -c | ${SORT} -rn | ${HEAD} -n${REPORT_LINES})
+# shellcheck disable=SC2016
 TL=$(echo "${QUERIES}" | ${AWK} '{print $6}' | ${SORT} | ${UNIQ} -c | ${SORT} -rn | ${HEAD} -n${REPORT_LINES})
+# shellcheck disable=SC2016
 TC=$(echo "${QUERIES}" | ${AWK} '{print $NF}' | ${SORT} | ${UNIQ} -c | ${SORT} -rn | ${HEAD} -n${REPORT_LINES})
 
 #cat << END
